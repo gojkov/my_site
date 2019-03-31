2901762a5a87111087537fe95c6e029c57ded149
Steve Gojkov <stevegojkov@gmail.com>
2019-03-31 18:12:43 -0400

    Initial site transfer
    Putting site files on Git

---
+++ js/stevehome.js
@@ -0,0 +1,31 @@
+$("h1 span").on("mouseover", function(){
+    $(this).css("color", "#d4af37");
+});
+
+$("h1 span").on("mouseout", function(){
+    $(this).css("color", "ivory");
+});
+
+$("ion-icon").on("mouseover", function(){
+    $(this).css("color", "#d4af37");
+});
+
+$("ion-icon").on("mouseout", function(){
+    $(this).css("color", "ivory");
+});
+
+$(".nav-item").on("mouseover", function(){
+    $(this).css("color", "#d4af37");
+});
+
+$(".nav-item").on("mouseout", function(){
+    $(this).css("color", "ivory");
+});
+
+$(".navbar-brand").on("mouseover", function(){
+    $(this).css("transform", "scale(1.4)");
+});
+
+$(".navbar-brand").on("mouseout", function(){
+    $(this).css("transform", "scale(1)");
+});
---
+++ stevehome.js
@@ -0,0 +1,7 @@
+$("h1 span").on("mouseover", function(){
+    $(this).css("color", "#d4af37");
+});
+
+$("h1 span").on("mouseout", function(){
+    $(this).css("color", "ivory");
+});
